a8678ff8a022bbb477aa7657bd8cf58d91509ff6
Sunghoon Cho <exer@iMac-von-Sunghoon.fritz.box>
2025-08-02 16:51:50 +0200

    Set host to 0.0.0.0 for Render deployment

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def check_vat():
         return jsonify({"error": "Interner Fehler bei der Anfrage."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    #app.run(debug=True)
